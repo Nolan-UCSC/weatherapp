e969b0e6cb6c6d9ac5ed1ae8e63747a3a2a3111d
Nolan Jensen <130103322+Nolan-UCSC@users.noreply.github.com>
2023-04-26 19:09:06 -0700

    updating

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // Create the variable for latitude
+let latitude = 0;
 // Create the variable for longitude
+let longitude = 0;
 
 window.onload = function() {
     const date = new Date();
-    const dateString = (date.getMonth() + 1) + '/' + date.getDate() +      '/' + date.getFullYear();
+    const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
     // Now, set the .date HTML text to our dateString
-    if ("geolocation" in navigator) {
-		navigator.geolocation.getCurrentPosition(success)
+    document.getElementById('date').innerHTML = dateString;
+    if ("geolocation" in navigator) { //if location supported then
+		navigator.geolocation.getCurrentPosition(success) //get pos and calls success
 
-	} else {
+	} else { //if location does not exist or denied then
 	  console.log("Geolocation is not available in your browser.");
 	}    
 }
 function success(position){
 	latitude = position.coords.latitude;
 	longitude = position.coords.longitude;
+    console.log(latitude, " ", longitude);
 	// Print out the latitude and longitude to see if it works!
     
 }
